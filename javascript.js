d5ef1b3032d0cfd76d56d8490a562a6a7be4613e
ImJithinnnnn <jithinsoorya123@gmail.com>
2026-01-11 10:49:42 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+
+function quantities(){ 
+    var q="quantities =" + document.getElementById("quantity").value + " name =" + document.getElementById("fname").value + " Phone no:" + document.getElementById("num").value + " pincode:" + document.getElementById("Pincode").value + " Address:" + document.getElementById("Address").value
+    if(document.getElementById("quantity").value>0){
+        console.log(q);
+        localStorage.setItem(document.getElementById("fname").value, q);
+    }
+    else{alert("Select how many of you want NIGGESH")}
+}
+
+
+console.log("script running")
