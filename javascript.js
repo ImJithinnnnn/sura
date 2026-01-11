8ddb2575a53033995a6fc88437489066a5bffb50
ImJithinnnnn <jithinsoorya123@gmail.com>
2026-01-11 11:10:08 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,11 +4,12 @@ function quantities(){
     var q="quantities =" + document.getElementById("quantity").value + " name =" + document.getElementById("fname").value + " Phone no:" + document.getElementById("num").value + " pincode:" + document.getElementById("Pincode").value + " Address:" + document.getElementById("Address").value
     if(document.getElementById("quantity").value>0){
         console.log(q);
-        sessionstorage.setItem(document.getElementById("fname").value, q);
+        localstorage.setItem(document.getElementById("fname").value, q);
     }
     else{alert("Select how many of you want NIGGESH")}
 }
 
 
 console.log("script running")
+
 
